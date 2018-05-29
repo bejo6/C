ca1ebfe8ac749ba8fbbddb0e6b35d7dea983fff2
bejo6 <bejo6.er@gmail.com>
2018-05-30 01:01:05 +0700

    Create mkdir_recursive.c

---
+++ mkdir_recursive.c
@@ -0,0 +1,47 @@
+/*
+Make Directory Recursive
+Credit : http://niallohiggins.com/2009/01/08/mkpath-mkdir-p-alike-in-c-for-unix/
+*/
+
+#include <libgen.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int mkpath(const char *s, mode_t mode){
+   char *q, *r = NULL, *path = NULL, *up = NULL;
+   int rv;
+   
+   rv = -1;
+   if (strcmp(s, ".") == 0 || strcmp(s, "/") == 0) return (0);
+   		
+   if ((path = strdup(s)) == NULL) exit(1);
+   
+   if ((q = strdup(s)) == NULL) exit(1);
+   		
+   if ((r = dirname(q)) == NULL) goto out;
+   
+   if ((up = strdup(r)) == NULL) exit(1);
+   
+   if ((mkpath(up, mode) == -1) && (errno != EEXIST)) goto out;
+   		
+   if ((mkdir(path, mode) == -1) && (errno != EEXIST)){
+   	rv = -1;
+   }else{
+   	rv = 0;
+   }
+
+   out:
+	if (up != NULL) free(up);
+			
+	free(q);
+	free(path);
+	return (rv);
+}
+
+int main(int argc,char **argv){
+  if (argc > 1){
+     mkpath(argv[1], 0755);
+  }
+  return 0;
+}
